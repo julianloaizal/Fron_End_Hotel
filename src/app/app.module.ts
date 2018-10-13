463dc249c0810692eaeac86a5e0776db642e6607
camiloac2 <arley.fuentesa@udea.edu.co>
2018-10-13 13:46:53 -0500

    Creación de los paquetes de la capa vista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,27 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { RegistrousuarioComponent } from './registrousuario/registrousuario.component';
+import { ReservacionComponent } from './reservacion/reservacion.component';
+import { PagoComponent } from './pago/pago.component';
+import { FacturaComponent } from './factura/factura.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyComponent,
-    FooterComponent
+    FooterComponent,
+    MenuComponent,
+    UsuarioComponent,
+    RegistrousuarioComponent,
+    ReservacionComponent,
+    PagoComponent,
+    FacturaComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule
