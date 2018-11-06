7a5f2e0ddefc0ddd5ef0b46f2a10ba8411187d17
Julian Loaiza <julian.loaizal@udea.edu.co>
2018-11-05 22:39:57 -0500

    Creación de rutas para las historias de usuarios seleccionadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import {RouterModule, Routes } from '@angular/router';
 import { Component } from '@angular/core';
-import { BodyComponent } from './components/body/body.component';
-import { ReservacionComponent } from './components/reservacion/reservacion.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { LoginComponent } from './components/login/login.component';
 
 const APP_ROUTES: Routes = [
-    { path: 'home', component: BodyComponent},
-    { path: 'reservacion', component: ReservacionComponent},
+    { path: 'home', component: HomeComponent},
+    { path: 'registro', component: RegistroComponent},
+    { path: 'login', component: LoginComponent},
     { path: '**', pathMatch: 'full', redirectTo: '' }
 ];
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
