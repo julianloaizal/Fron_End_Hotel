842f97886f00a458aa0847ec90cc89e53719b51a
Julian Loaiza <julian.loaizal@udea.edu.co>
2018-11-06 00:48:40 -0500

    Configuración modulo reservaciones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import {RouterModule, Routes } from '@angular/router';
 import { Component } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
+import { ReservacionComponent } from './components/reservacion/reservacion.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { LoginComponent } from './components/login/login.component';
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent},
+    { path: 'reservacion', component: ReservacionComponent},
     { path: 'registro', component: RegistroComponent},
     { path: 'login', component: LoginComponent},
     { path: '**', pathMatch: 'full', redirectTo: '' }
